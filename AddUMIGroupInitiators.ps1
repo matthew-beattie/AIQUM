a66cd8d4e540dd0446baecd0f60057df754280d5
matthew-beattie <56328734+matthew-beattie@users.noreply.github.com>
2020-05-29 15:35:21 +1000

    Create AddUMIGroupInitiators.ps1
    Syntax:
    
    Add Initiators to an IGroup by the IGroup, Cluster, Vserver name:
    
    PS C:\Scripts\PowerShell\Projects\AIQUM> .\AddUMIGroupInitiators.ps1 -Server 'aiq.demo.netapp.com' -IGroupName 'jumphost_igroup' -ClusterName 'cluster2' -VserverName 'svm21' -Initiators @("iqn.1991-05.com.microsoft:server01.demo.netapp.com"
    ,"iqn.1991-05.com.microsoft:server02.demo.netapp.com") -Credential $Credential
    
    Add Initiators to an IGroup by -IGroupID (Resource Key):
    
    PS C:\Scripts\PowerShell\Projects\AIQUM> .\AddUMIGroupInitiators.ps1 -Server 'aiq.demo.netapp.com' -IGroupID "2f3c6e9c-acfc-11e9-8271-005056b03109:type=igroup,uuid=6f1c9a84-ad05-11e9-8271-005056b03109" -Initiators @("iqn.1991-05.com.microso
    ft:server03.demo.netapp.com","iqn.1991-05.com.microsoft:server04.demo.netapp.com") -Credential $Credential

---
+++ AddUMIGroupInitiators.ps1
@@ -0,0 +1,60 @@
+Param(
+   [Parameter(Mandatory = $True, HelpMessage = "The AIQUM server Hostname, FQDN or IP Address")]
+   [ValidateNotNullOrEmpty()]
+   [String]$Server,
+   [Parameter(Mandatory = $False, HelpMessage = "The IGroup Name")]
+   [String]$IGroupName,
+   [Parameter(Mandatory = $False, HelpMessage = "The IGroup Resource Key. The syntax is: 'key=<uuid>:type=<object_type>,uuid=<uuid>'")]
+   [String]$IGroupID,
+   [Parameter(Mandatory = $False, HelpMessage = "The Cluster Name")]
+   [String]$ClusterName,
+   [Parameter(Mandatory = $False, HelpMessage = "The Vserver Name")]
+   [String]$VserverName,
+   [Parameter(Mandatory = $True, HelpMessage = "The IGroup Name")]
+   [Array]$Initiators,
+   [Parameter(Mandatory = $True, HelpMessage = "The Credential to authenticate to AIQUM")]
+   [ValidateNotNullOrEmpty()]
+   [System.Management.Automation.PSCredential]$Credential
+)
+#'------------------------------------------------------------------------------
+#'Import the AIQUM Module.
+#'------------------------------------------------------------------------------
+Import-Module .\AIQUM.psm1
+Write-Host "Imported Module .\AIQUM.psm1"
+#'------------------------------------------------------------------------------
+#'Set the certificate policy and TLS version.
+#'------------------------------------------------------------------------------
+Add-Type @"
+   using System.Net;
+   using System.Security.Cryptography.X509Certificates;
+   public class TrustAllCertsPolicy : ICertificatePolicy {
+   public bool CheckValidationResult(
+   ServicePoint srvPoint, X509Certificate certificate,
+   WebRequest request, int certificateProblem) {
+      return true;
+   }
+}
+"@
+[System.Net.ServicePointManager]::SecurityProtocol  = [System.Net.SecurityProtocolType]'Tls12'
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+#'------------------------------------------------------------------------------
+#'Set the command to add the initiators to the IGroup.
+#'------------------------------------------------------------------------------
+[String]$command = "Add-UMIGroupInitiators -Server $Server "
+If($IGroupID){
+   [String]$command += "-IGroupID '$IGroupID' -Initiators `$Initiators -Credential `$Credential -ErrorAction Stop"
+}Else{
+   [String]$command += "-IGroupName $IGroupName -ClusterName $ClusterName -VserverName $VserverName -Initiators `$Initiators -Credential `$Credential -ErrorAction Stop"
+}
+#'------------------------------------------------------------------------------
+#'Add the initiators to the IGroup.
+#'------------------------------------------------------------------------------
+Try{
+   $i = Invoke-Expression -Command $command -ErrorAction Stop
+   Write-Host "Executed Command`: $command" -ForegroundColor Cyan
+   $i.records
+}Catch{
+   Write-Warning -Message $("Failed Executing Command`: $command. Error " + $_.Exception.Message)
+   $i
+}
+#'------------------------------------------------------------------------------
